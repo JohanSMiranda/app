aabec6a25279f9e385e4f04ad662c5a52883a9ec
Johan Miranda <sm-9909@outlook.com>
2024-10-04 04:48:24 +0000

    Create role crud and config.

---
+++ app/models/profile.rb
@@ -0,0 +1,24 @@
+class Profile < ApplicationRecord
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :birth, presence: true
+  validates :gender, presence: true
+  validate :validate_gender
+  belongs_to :user
+
+  enum role: [:member, :admin, :superadmin]
+
+  after_create :set_role 
+
+  private
+  def validate_gender
+    unless self.gender == 'Male' || self.gender == 'Female'
+      errors.add(:gender, "Was specified incorrectly")
+    end
+  end
+
+  def set_role
+    self.update(role: 0)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_01_033019) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_01_044329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_01_033019) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.string "country"
+    t.date "birth"
+    t.string "gender"
+    t.integer "role"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -31,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_01_033019) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
